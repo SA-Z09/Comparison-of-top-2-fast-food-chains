49ec594499d7634f97233f181a78821b33cad683
Shreya Zope <92116917+SA-Z09@users.noreply.github.com>
2022-07-12 23:26:34 +0530

    Update StarbucksLocation.py

--- StarbucksLocation.py
+++ StarbucksLocation.py
@@ -2,3 +2,18 @@ import pandas as pd
 import os
 import requests
 from bs4 import BeautifulSoup
+htmlcontent = requests.get("https://en.wikipedia.org/wiki/Starbucks#Locations")
+bs = BeautifulSoup(htmlcontent.text, 'html.parser')
+bs.find_all('table',
+attrs={'style':'width:100%;'})
+extract = bs.find_all('table', attrs={'style':'width:100%;'})
+final_extract = extract[0].find_all('li')
+final_extract
+len(final_extract)
+title_list = []
+for i in range(len(final_extract)):
+    title_list.append(final_extract[i].a['title'],final_extract[i].strip(':').strip())
+    final_extract[i].text
+    title_list
+    df = pd.DataFrame(title_list=)
+    df
